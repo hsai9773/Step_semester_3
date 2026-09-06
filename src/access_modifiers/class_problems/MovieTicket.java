e89d53730643950b7a9b09a029b51254581f9b88
hsai9773 <hsai9773@gmail.com>
2026-09-06 19:14:36 +0530

    Remove unused access_modifiers package

--- src/access_modifiers/class_problems/MovieTicket.java
+++
@@ -1,8 +0,0 @@
-package access_modifiers.class_problems;
-
-public class MovieTicket {
-    private String seatNumber;
-    protected String screenId;
-    double ticketPrice;
-    public String movieTitle;
-}
